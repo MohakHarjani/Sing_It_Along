9e4c649e6a461f11339678ece5eb68e00dc11a09
MohakHarjani <65814030+MohakHarjani@users.noreply.github.com>
2022-04-22 13:19:48 +0530

    Add files via upload

---
+++ sing_it_along/views/src/containers/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Register } from '../components/Register';
+export const Home = ()=>{
+    return (
+        <>
+            <h1 style={{marginBottom : '0px'}}className = 'alert-primary text-center'>SING IT ALONG</h1>
+            <Register/>
+        </>
+
+    )
+}
